01ca86be8e8c34545783b534431c116cc90f9d6f
Daniel Rafael <dara6996@colorado.edu>
2020-10-14 12:00:46 -0600

    Shmup Remix

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    private int score;
+    public static GameManagerScript S;
+    
+    void Awake()
+    {
+        S = this;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score == 20)
+        {
+            GameWinner();
+        }
+    }
+    
+    public void IncreaseScore() { //switched from private to public to allow HoleScript use of this function - 13th mistake found
+        score++;
+    }
+
+    public void GameOver()
+    {
+        //GameOver
+        SceneManager.LoadScene("GameOver");
+    }
+    
+    public void GameWinner()
+    {
+        //GameOver
+        SceneManager.LoadScene("GameWinner");
+    }
+}
